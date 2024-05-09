f146708a7bf4d0754e32a75a664f7f955eda075d
Altan Orhon <altan@uw.edu>
2024-05-08 23:20:10 -0700

    Cleanup

--- tutorial/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-wget -O dataset.zip https://github.com/talmolab/sleap-datasets/releases/download/dm-courtship-v1/drosophila-melanogaster-courtship.zip
-mkdir dataset
-unzip dataset.zip -d dataset
-rm dataset.zip
